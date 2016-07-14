e20a06da368f71af17b47c63288b8c767ec04205
Luis Villasenor <luis@iceberg9.com>
2016-07-14 14:11:15 -0500

    Actualiza controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < AuthenticatedController
   def index
-    #@products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
-    @products = ShopifyAPI::Product.where({ title : "SA" })
+    @products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
   end
 end
