76b1ce29e4f664d18e5059b0610fd7b08e973c9e
Luis Villasenor <luis@iceberg9.com>
2016-07-15 11:19:48 -0500

    Obtiene las Ordenes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < AuthenticatedController
   def index
-  	#@products = ShopifyAPI::Product.find(:all, :params => {:title => "SA", :limit => 10})
+  	#@products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
     #@products = ShopifyAPI::Product.find(:all, :params => {:title => "SA", :limit => 10})
-    @products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
+    @orders = ShopifyAPI::Order.find(:all, :params => {:limit => 10})
   end
 end
