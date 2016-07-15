8c8cdf88199cd68b69d48845bdc44ad7436c9eb0
Luis Villasenor <luis@iceberg9.com>
2016-07-15 10:43:37 -0500

    Actualiza consulta

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < AuthenticatedController
   def index
   	#@products = ShopifyAPI::Product.find(:all, :params => {:title => "SA", :limit => 10})
-    @products = ShopifyAPI::Product.find(:all, :params => {:title => "SA", :limit => 10})
+    #@products = ShopifyAPI::Product.find(:all, :params => {:title => "SA", :limit => 10})
+    @products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
   end
 end
